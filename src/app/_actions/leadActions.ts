ca2f9a90ba178719e658544aa6dc9bef1889e6cc
Caken Tan <cakent@capc.com.sg>
2025-05-15 15:29:27 +0800

    test

--- src/app/_actions/leadActions.ts
+++ src/app/_actions/leadActions.ts
@@ -173,6 +173,7 @@ export async function createLead(input: CreateLeadInput) {
   }
 }
 
+
 // Import multiple leads from an Excel file
 export async function importLeads(leadsData: any[]) {
   const { userId } = await auth();
